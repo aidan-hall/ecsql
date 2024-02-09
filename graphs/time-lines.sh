8700d28a6a6fab4fb0d0485720e60a62b0d41c42
Aidan Hall <aidan.hall@warwick.ac.uk>
2024-02-09 12:30:32 +0000

    graphs/time-lines.*: Work relative to graphs/

--- graphs/time-lines.sh
+++ graphs/time-lines.sh
@@ -4,6 +4,6 @@ git --no-pager log --pretty='format:%at %H' --reverse \
     | awk '{printf "%s ", $1; system("git checkout -q " $2);
             system("cloc --vcs=git --csv | grep SUM | cut -d, -f5");}' \
                 | sed 1d \
-                       > time-lines.txt
-./time-lines.gnu
+                       > graphs/time-lines.txt
+graphs/time-lines.gnu
 git checkout main
