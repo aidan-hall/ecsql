a82744c4c2f342d2e3fbf47d6304b1eececbeef6
Aidan Hall <aidan.hall@warwick.ac.uk>
2024-02-19 14:26:53 +0000

    lisp/ecs.lisp: Reformatted

--- lisp/ecs.lisp
+++ lisp/ecs.lisp
@@ -1,13 +1,13 @@
 (defmacro defcomponent (name type)
   `(if (ecs-get-by-name ',name)
        (wrong "An entity with the given name already exists" ',name)
-       (let ((comp (ecs-new-component ',type)))
-         ;; Attempt to set the name inside the ECS first, since this could fail.
-         (if (ecs-set-name comp ',name)
-             (defvar ,name comp)
-             (puts "Couldn't set entity name. Cleaning up....")
-             ;; Clean up the Entity if we failed to assign the name.
-             (ecs-destroy comp)))))
+     (let ((comp (ecs-new-component ',type)))
+       ;; Attempt to set the name inside the ECS first, since this could fail.
+       (if (ecs-set-name comp ',name)
+           (defvar ,name comp)
+         (puts "Couldn't set entity name. Cleaning up....")
+         ;; Clean up the Entity if we failed to assign the name.
+         (ecs-destroy comp)))))
 
 (defcomponent Pos v3)
 (defcomponent Vel v3)
