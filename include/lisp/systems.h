b8bc1a3dc382e707df10c29f8760aba78b2daf1a
Aidan Hall <aidan.hall@warwick.ac.uk>
2024-02-28 17:47:24 +0000

    src/lisp/systems.c: + run_matching_{self_join_}...

--- include/lisp/systems.h
+++ include/lisp/systems.h
@@ -5,6 +5,8 @@
 #include <ecs/query.h>
 
 void lisp_run_system(LispEnv *lisp, struct EcsIter *iter, void *data);
+void run_matching_systems(LispEnv *lisp, struct EcsIter *iter, void *data);
+void run_matching_self_join_systems(LispEnv *lisp, struct EcsIter *iter, void *data);
 
 
 #endif
--- src/lisp/systems.c
+++ src/lisp/systems.c
@@ -11,3 +11,26 @@ void lisp_run_system(LispEnv *lisp, struct EcsIter *iter, void *data) {
     lisp_apply(lisp, system, args_form);
   }
 }
+
+void run_matching_systems(LispEnv *lisp, struct EcsIter *iter, void *data) {
+  IGNORE(data);
+  size N = ecs_iter_count(iter);
+  SystemFunc **functions = ecs_iter_get(iter, 0);
+  Object *queries = ecs_iter_get(iter, 1);
+  void **datas = ecs_iter_get(iter, 2);
+  for (size i = 0; i < N; ++i) {
+    ecs_do_query(lisp, queries[i], functions[i], datas[i]);
+  }
+}
+
+void run_matching_self_join_systems(LispEnv *lisp, struct EcsIter *iter,
+                                    void *data) {
+  IGNORE(data);
+  size N = ecs_iter_count(iter);
+  NWiseSystem *functions = ecs_iter_get(iter, 0);
+  Object *queries = ecs_iter_get(iter, 1);
+  void **datas = ecs_iter_get(iter, 2);
+  for (size i = 0; i < N; ++i) {
+    ecs_do_pairwise_query(lisp, queries[i], queries[i], functions[i], datas[i]);
+  }
+}
