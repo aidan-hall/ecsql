b4e3eeb1e1d83146abb8dc61a2ca6ab762cb5304
Aidan Hall <aidan.hall@warwick.ac.uk>
2024-02-28 18:14:32 +0000

    examples/*.lisp: Extended eval day demo

---
+++ examples/planets-components.lisp
@@ -0,0 +1,15 @@
+(defvar col-red (make-colour 230 41 55 255)) ; Raylib red
+(defvar col-green (make-colour 0 255 0 255))
+(defcomponent Species nil)
+(defcomponent Wizard nil)
+(defcomponent Human nil)
+
+(ecs-add* (defcomponent Dwarf nil)
+          (Colour = col-red)
+          (Radius 15.0))
+(ecs-add* (defcomponent Elf nil)
+          (Colour 0 0 255 255)          ; Can also use literal values.
+          (Radius 10.0))
+(ecs-add* (defcomponent Goblin nil)
+          (Colour = col-green)
+          (Radius 5.0))
