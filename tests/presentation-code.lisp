ae7cca49c4ed5204c2269f658f29ddb07f878f60
Aidan Hall <aidan.hall@warwick.ac.uk>
2024-03-14 20:18:18 +0000

    tests/presentation-code.lisp: Fixes & formatting

--- tests/presentation-code.lisp
+++ tests/presentation-code.lisp
@@ -1,4 +1,8 @@
+;; Define 
 (load "examples/planets-components.lisp")
+
+
+
 
 ;; Create an Entity and poke it about
 (defvar e
@@ -8,7 +12,15 @@
             (Radius 10.0)
             (Colour 255 0 0 255)))
 
+
+(ecs-add* e (Bounce 0.8))
+
+
+
+;; Load a complex pre-defined scene.
 (load "examples/planets-scene.lisp")
+
+
 
 ;; Make all moving Entities move towards (200,200)
 (ecsql (and Pos Vel)
@@ -17,6 +29,8 @@
                (- 200 (v2-x pos))
                (- 200 (v2-y pos))))
 
+
+
 ;; Colour all Elves and Dwarves yellow
 (ecsql (and Colour
             (has (or Dwarf Elf)))
@@ -24,14 +38,16 @@
        (set-colour colour
                    255 255 0 255))
 
-;; See the definition of the Move system in main.c
-(ecs-lookup 'Move)
-(ecs-destroy (ecs-lookup 'Move))
+
+
+;; Define the same System in C and Lisp
+(ecs-resolve 'Move)
+(ecs-destroy (ecs-resolve 'Move))
+
 
 (defvar move-system
   (ecs-new-system
-   (Physics
-    (name Move))
+   (Physics (name 'Move))
    (and Pos Vel)
    (pos vel)
    (let ((delta (get-delta)))
@@ -40,5 +56,56 @@
              (+ (v2-y pos) (* (v2-y vel) delta))))))
 
 
-;; Start up some of the systems in examples/systems.lisp and show them off
+;; More Lisp Systems
 
+(defvar wizard-follow
+  (ecs-new-system
+   (Physics (name 'WizardFollow))
+   (and (has Wizard) Pos) (pos)
+   (set-v2 pos
+           (+ (get-mouse-x) 30.0) (float (get-mouse-y)))))
+
+
+(defvar pos-label
+  (ecs-new-system
+   (Graphics)
+   (and Pos Radius (has Colour) (has Vel)) (pos radius)
+   (draw-text (to-string
+               (list (floor (v2-x pos)) (floor (v2-y pos))))
+              (+ (v2-x pos) radius)
+              (- (v2-y pos) radius) 8)))
+
+
+(defun point-in-circle (centre radius point)
+  "Test whether POINT is inside a circle."
+  (let* ((radius2 (* radius radius))
+         (diff-x (- (v2-x centre) (v2-x point)))
+         (diff-x2 (* diff-x diff-x))
+         (diff-y (- (v2-y centre) (v2-y point)))
+         (diff-y2 (* diff-y diff-y)))
+    (and (< diff-x2 radius2) (< diff-y2 radius2))))
+
+
+(defvar clicked-me
+  (ecs-new-system
+   (Graphics)
+   (and Pos Radius (has Colour) (has Vel)) (pos radius)
+   (when (and (is-mouse-down 'left)
+              (point-in-circle
+               pos radius
+               (make-v2 (float (get-mouse-x))
+                        (float (get-mouse-y)))))
+     (draw-text (concat "Clicked: "
+                        (to-string (ecs-id entity)))
+                10. 10. 20))))
+
+(defvar kill-click
+  (ecs-new-system
+   (Physics)
+   (and Pos Radius (has Colour) (has Vel)) (pos radius)
+   (when (and (is-mouse-pressed 'right)
+              (point-in-circle
+               pos radius
+               (make-v2 (float (get-mouse-x))
+                        (float (get-mouse-y)))))
+     (ecs-destroy entity))))
